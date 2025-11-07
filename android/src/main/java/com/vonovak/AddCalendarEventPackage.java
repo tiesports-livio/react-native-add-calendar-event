b1883450030a9d95ea83435998cb4d5e12b713fe
Lívio <livio@tiesports.com>
2025-11-07 14:35:27 +0000

    Add support for react-native 0.82.1

--- android/src/main/java/com/vonovak/AddCalendarEventPackage.java
+++ android/src/main/java/com/vonovak/AddCalendarEventPackage.java
@@ -9,15 +9,11 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
+
 public class AddCalendarEventPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
       return Arrays.<NativeModule>asList(new AddCalendarEventModule(reactContext));
-    }
-
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
     }
 
     @Override
--- react-native-add-calendar-event.podspec
+++ react-native-add-calendar-event.podspec
@@ -8,7 +8,7 @@ Pod::Spec.new do |s|
   s.homepage     = 'https://github.com/vonovak/react-native-add-calendar-event'
   s.author       = package_json['author']
   s.license      = package_json['license']
-  s.platform     = :ios, '8.0'
+  s.platform     = :ios, '12.4'
   s.source       = { :git => 'https://github.com/vonovak/react-native-add-calendar-event.git', :tag => "v#{s.version}" }
 
   s.source_files  = 'ios/*.{h,m}'
